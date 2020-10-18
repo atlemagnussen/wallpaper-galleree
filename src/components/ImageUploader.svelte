3a0d64171b0d82a4005415e1a364e881868e0f19
Atle Magnussen <atlemagnussen@gmail.com>
2020-10-18 14:31:04 +0200

    trim

--- src/components/ImageUploader.svelte
+++ src/components/ImageUploader.svelte
@@ -19,19 +19,14 @@
         console.log("onDrop");
 
         if (ev.dataTransfer.items) {
-            // Use DataTransferItemList interface to access the file(s)
             for (let i = 0; i < ev.dataTransfer.items.length; i++) {
-                // If dropped items aren't files, reject them
                 if (ev.dataTransfer.items[i].kind === 'file') {
                     let file = ev.dataTransfer.items[i].getAsFile();
-                    console.log('... file[' + i + '].name = ' + file.name);
                     uploadFile(file)
                 }
             }
         } else {
-            // Use DataTransfer interface to access the file(s)
             for (let i = 0; i < ev.dataTransfer.files.length; i++) {
-                console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
                 uploadFile(file);
             }
         }
